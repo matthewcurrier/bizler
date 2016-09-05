189b440d221a7ba33153c47bd48e04d00f077850
Matthew Currier <currier.matthew@gmail.com>
2016-09-04 18:57:59 -0700

    Added zoo file for experimentation.

--- R/zoo.R
+++ R/zoo.R
@@ -6,3 +6,14 @@ lag(myts, k=1)
 
 #calculate % difference
 newts <- diff(myts, 12) / myts
+
+
+finalts <- cbind(myts, newts)
+
+
+df <- yearmon(finalts)
+
+myts <- as.zoo(myts)
+
+#This will take monthly zoo object and aggregate it over quarter
+x2a <- aggregate(myts, as.Date(as.yearqtr(time(myts))), sum)
