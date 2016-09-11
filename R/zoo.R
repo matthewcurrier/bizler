d08556b3c258e7f0f48dc5fcf1bc19a58177fe08
Matthew Currier <currier.matthew@gmail.com>
2016-09-11 10:56:56 -0700

    Created mechanism to fill in data with zeros for noncontiguous dates.

--- R/zoo.R
+++ R/zoo.R
@@ -2,10 +2,10 @@ library(MASS)
 library(zoo)
 myts <- USAccDeaths
 
-lag(myts, k=1)
+lag(myts, k = 1)
 
-#calculate % difference
-newts <- diff(myts, 12) / myts
+# calculate % difference
+newts <- diff(myts, 12)/myts
 
 
 finalts <- cbind(myts, newts)
@@ -15,14 +15,16 @@ df <- yearmon(finalts)
 
 myts <- as.zoo(myts)
 
-#This will take monthly zoo object and aggregate it over quarter
+# This will take monthly zoo object and aggregate it over
+# quarter
 x2a <- aggregate(myts, as.Date(as.yearqtr(time(myts))), sum)
 
 
 
 
-dates <- as.POSIXct( c('2015-11-01', '2016-01-01', '2015-04-30','2015-09-01', '2015-12-01'))
-as.numeric(as.yearmon(dates) + 2/12) %/% 1
+dates <- as.POSIXct(c("2015-11-01", "2016-01-01", "2015-04-30",
+  "2015-09-01", "2015-12-01"))
+as.numeric(as.yearmon(dates) + 2/12)%/%1
 
 # How To Split A Data Set Up
 g <- airquality$Month
---
+++ R/zoo2.R
@@ -0,0 +1,52 @@
+library(tidyr)
+#test <- as.zoo(nyc_bike_crashes)
+#test2 <- read.zoo()
+
+#Taken from http://r.789695.n4.nabble.com/building-time-series-zoo-its-from-a-data-frame-td2240349.html
+makezoo <- function(df) {
+  require(zoo)
+  with(df, zoo(total_accs_for_day, date))
+}
+
+#Combine borough and zip code for easier manipulations
+mydf <- nyc_bike_crashes %>% unite(groupings, borough, zip_code, sep="^_^")
+
+df_split_up <- split(mydf, mydf$groupings)
+head(df_split_up[[1]])
+
+
+#Select only data, groupings, and number_of_cyclist_injured. Working here on trying
+#to figure out just one example.
+df_split_up2 <- df_split_up[[1]] %>% select(date, groupings, total_cyclists_killed_injured)
+df_split_up2 <- df_split_up2 %>% select(-groupings)
+df_split_up2 <- df_split_up2 %>% group_by(date) %>% summarise(total_accs_for_day=sum(total_cyclists_killed_injured, na.rm=TRUE)) %>% ungroup()
+
+#Now you'll need to merge this data with an empty zoo object
+#find max and min so we can generate a sequence for merging
+start <- min(df_split_up2$date)
+end <- max(df_split_up2$date)
+tt <- seq(start, end, by = "day")
+
+myzoo <- makezoo(df_split_up2)
+
+
+
+myzootest <- merge(myzoo, zoo(,tt), fill = 0)
+
+
+#This seems to work
+myzoo2 <-aggregate(myzootest, as.yearmon, sum)
+
+
+#You should be able to compute lags from this point onwards
+
+
+
+# This will take monthly zoo object and aggregate it over
+# quarter
+myzoo2 <- aggregate(myzoo2, as.Date(as.yearqtr(time(myzoo2))), sum)
+
+
+
+#Now let's convert to a zoo object
+df_as_zoo <- read.zoo(df_split_up2)
